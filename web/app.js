7861dfcf6de987f4ac8969254538a6b8b5b17a4b
Steven Brown <Steven Brown>
2017-02-24 23:58:13 -0500

    early layout

--- web/app.js
+++ web/app.js
@@ -3,14 +3,16 @@ const app = express() ;
 const db = require('../db.js');
 
 app.set('view engine', 'pug')
-app.get('/', function (req, res) {  
+app.locals.pretty = true;
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
 	db.getAllSounds()
 	.then(function(listSounds) {
 		res.render('index', { sounds: listSounds})
 	});
-    
+
 })
 
-app.listen(3000, function () {  
+app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
