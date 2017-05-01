f459525e043704f86401205a83af725714093bd8
Steve <scbDeveloper@gmail.com>
2017-05-01 19:42:09 -0400

    set up testing framework slightly

---
+++ web/sound-list/sound-list-component.spec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('soundList', function() {
+
+  // Load the module that contains the `phoneList` component before each test
+  beforeEach(angular.mock.module('soundList'));
+
+  // Test the controller
+  describe('SoundListController', function() {
+
+
+    it('should fucking work', function() {
+
+      expect(2).toEqual(2);
+    });
+
+  });
+
+});
