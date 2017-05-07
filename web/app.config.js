7edb1b0dab6dfd84ec543227e6e89b229f263633
Steve <scbDeveloper@gmail.com>
2017-05-07 14:21:48 -0400

    moved app config to separate file

--- web/app.config.js
+++ web/app.config.js
@@ -1,3 +1,12 @@
 'use strict';
 
-angular.module('jacquesApp')
+angular
+    .module('jacquesApp')
+    .config(($mdIconProvider, $mdThemingProvider) => {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('green')
+            .accentPalette('deep-orange');
+        $mdIconProvider
+            .icon("menu", "assets/svg/menu.svg", 24)
+            .icon("invite", "assets/svg/person_add.svg", 24);
+    });
