0bf3d7150290c9ea1fd0198e97ee244acb36cd72
Steve <scbDeveloper@gmail.com>
2017-05-14 17:56:56 -0400

    db connect

--- web/birdfeed.js
+++ web/birdfeed.js
@@ -18,6 +18,8 @@ app.use(bodyParser.json());
 var port = 8080;
 var router = express.Router();
 
+db.connect();
+
 router.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
@@ -36,17 +38,12 @@ router.get('/', function(req, res) {
 router.route('/sounds')
     .get(function(req, res) {
         logger.info("Received request for sounds.");
-        console.log("Received request for sounds.");
         db.getAllSounds()
             .then(function(sounds) {
                 if (sounds) {
-                    logger.info("Got sounds: " + sounds);
-                    console.log("Got sounds: " + sounds);
                     res.json(sounds);
                 } else {
-                    console.log("Now you fucked up");
                     logger.info("Now you fucked up");
-                    res.send("Now you fucked up");
                 }
             }).catch(logger.error)
     })
@@ -65,8 +62,6 @@ router.route('/sounds/:sound_name')
             res.json(sound);
         });
     });
-
-
 
 app.use('/api', router);
 app.get('/', function(req, res) {
