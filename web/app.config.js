94d3e5b2063c5a5af3a75343e0b8b5fedb44c708
scb5304 <scb5304@gmail.com>
2017-05-15 16:53:30 -0400

    help md icon

--- web/app.config.js
+++ web/app.config.js
@@ -8,6 +8,7 @@ angular
             .accentPalette('deep-orange');
         $mdIconProvider
             .icon("menu", "assets/svg/menu.svg", 24)
+            .icon("help", "assets/svg/help-circle-outline.svg", 24)
             .icon("github", "assets/svg/github-circle.svg", 24)
             .icon("invite", "assets/svg/person_add.svg", 24);
     });
