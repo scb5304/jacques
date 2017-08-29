b268fc50505a2d619f28886f22541fa8fe102b7c
scb5304 <scb5304@gmail.com>
2017-08-29 04:06:28 +0000

    removed confusing access control headers

--- web/birdfeed.js
+++ web/birdfeed.js
@@ -28,10 +28,6 @@ var router = express.Router();
 db.connect();
 
 router.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 });
 
