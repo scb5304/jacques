846a07236f7d7536e847bdb27b0afb52e450e9e1
scb5304 <scb5304@gmail.com>
2017-09-19 23:20:24 -0400

    Updated glob for client side files to include in coverage.

--- karma.conf.js
+++ karma.conf.js
@@ -44,7 +44,7 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: { "./web/**/!(spec)/*.js": ["coverage"] },
+    preprocessors: { "./web/**/!(*.spec).js": ["coverage"] },
 
 
     // test results reporter to use
