62936f7693c5bb92ef8beb8153ac59c6d378e224
scb5304 <scb5304@gmail.com>
2017-12-31 04:05:18 -0500

    I need this.

---
+++ jacques-api/statistics-controller.js
@@ -0,0 +1,24 @@
+const logger = require("../common/util/logger");
+const Db = require("../common/data/db");
+
+function getStatistics(req, res) {
+    var promises = [];
+    promises.push(Db.getGuildsCount());
+    promises.push(Db.getSoundsCount());
+    promises.push(Db.getSoundEventsCount());
+
+    Promise.all(promises)
+        .then(function(counts) {
+            var statisticsObject = {
+                guildsCount: counts[0],
+                soundsCount: counts[1],
+                soundEventsCount: counts[2],
+            };
+            res.json(statisticsObject);
+        }).catch(function(err) {
+            logger.error(err);
+            res.status(500).send({error: "Failed to get statistics: " + err});
+    });
+}
+
+module.exports.getStatistics = getStatistics;
