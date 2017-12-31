09efba18a229fef6c69d6654af3fb50ecd33a107
scb5304 <scb5304@gmail.com>
2017-12-31 03:05:04 -0500

    Put /assets in /shared

--- jacques-web/app/app.config.js
+++ jacques-web/app/app.config.js
@@ -7,20 +7,20 @@ angular
             .primaryPalette("green")
             .accentPalette("deep-orange");
         $mdIconProvider
-            .icon("view-list", "assets/svg/view-list.svg", 24)
-            .icon("menu", "assets/svg/menu.svg", 24)
-            .icon("help", "assets/svg/help-circle-outline.svg", 24)
-            .icon("github", "assets/svg/github-circle.svg", 24)
-            .icon("close", "assets/svg/close.svg", 24)
-            .icon("birdfeed", "assets/svg/hops.svg", 24)
-            .icon("chevron-up", "assets/svg/chevron-up.svg", 24)
-            .icon("invite", "assets/svg/person_add.svg", 24)
-            .icon("search", "assets/svg/magnify.svg", 24)
-            .icon("sort", "assets/svg/sort.svg", 24)
-            .icon("sound", "assets/svg/music-box.svg", 24)
-            .icon("delete", "assets/svg/delete-forever.svg", 24)
-            .icon("discord", "assets/svg/discord.svg", 24)
-            .icon("arrow-top-left", "assets/svg/arrow-top-left.svg", 24);
+            .icon("view-list", "shared/assets/svg/view-list.svg")
+            .icon("menu", "shared/assets/svg/menu.svg")
+            .icon("help", "shared/assets/svg/help-circle-outline.svg")
+            .icon("github", "shared/assets/svg/github-circle.svg")
+            .icon("close", "shared/assets/svg/close.svg")
+            .icon("birdfeed", "shared/assets/svg/hops.svg")
+            .icon("chevron-up", "shared/assets/svg/chevron-up.svg")
+            .icon("invite", "shared/assets/svg/person_add.svg")
+            .icon("search", "shared/assets/svg/magnify.svg")
+            .icon("sort", "shared/assets/svg/sort.svg")
+            .icon("sound", "shared/assets/svg/music-box.svg")
+            .icon("delete", "shared/assets/svg/delete-forever.svg")
+            .icon("discord", "shared/assets/svg/discord.svg")
+            .icon("arrow-top-left", "shared/assets/svg/arrow-top-left.svg");
         $urlRouterProvider.when('', '/');
         $urlMatcherFactoryProvider.strictMode(false);
     });
