b8149500a23a5ed951d687f97d69e70b34b2e60d
scb5304 <scb5304@gmail.com>
2018-01-06 14:19:37 -0500

    Unit test scroll-to-top.

---
+++ jacques-web/app/scroll-to-top/scroll-to-top.component.spec.js
@@ -0,0 +1,37 @@
+"use strict";
+
+describe("scrollToTop", function() {
+
+    beforeEach(function() {
+        module('ui.router');
+        module("scrollToTop");
+    });
+
+    describe("ScrollToTopController", function() {
+        var $scope;
+        var ScrollToTopController;
+
+        beforeEach(inject(function($componentController, $rootScope) {
+            $scope = $rootScope.$new();
+            ScrollToTopController = $componentController("scrollToTop", {
+                $scope: $scope
+            });
+        }));
+
+        fit("scrolls the page's main content to the very top when the FAB is pressed", function() {
+            var mockMainContent = {
+                scrollTo: function(x, y) {}
+            };
+
+            spyOn(document, "getElementById").and.callFake(function() {
+                return mockMainContent;
+            });
+
+            spyOn(mockMainContent, "scrollTo");
+
+            $scope.scrollToTop();
+            expect(ScrollToTopController).toBeDefined();
+            expect(mockMainContent.scrollTo).toHaveBeenCalledWith(0, 0);
+        });
+    });
+});
