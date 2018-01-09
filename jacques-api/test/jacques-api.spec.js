b99b2ea4c2d5386f4b523bc985edc50d0f847372
scb5304 <scb5304@gmail.com>
2018-01-08 21:51:45 -0500

    Removed boilerplate jacques-api tests.

--- jacques-api/test/jacques-api.spec.js
+++ jacques-api/test/jacques-api.spec.js
@@ -1,5 +1,20 @@
-require("./controller-test-utils");
-require("../jacques-api");
-require("../guild-controller");
-require("../statistics-controller");
-require("../user-controller");
+require("dotenv").config({path: require("app-root-path") + "/.env"});
+const sinon = require("sinon");
+const Db = require("../../common/data/db");
+
+beforeEach(function() {
+    this.sandbox = sinon.sandbox.create();
+});
+
+afterEach(function() {
+    this.sandbox.restore();
+});
+
+describe("jacques-api", function() {
+   it("connects to database, initializing without crashing", function(done) {
+       this.sandbox.stub(Db, "connect").callsFake(function() {
+           done();
+       });
+       require("../jacques-api");
+   })
+});
